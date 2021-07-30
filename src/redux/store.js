cf72dbf202e67c01761a209bc41e961a47e390c4
oxenprogrammer <emmy1000okello@gmail.com>
2021-07-30 22:26:04 +0300

    Feat(redux): add store

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, createStore } from "redux";
+
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
