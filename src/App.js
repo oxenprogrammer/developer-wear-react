abf1ebad61e64243044fb38f593a11cf4d2b7ae7
oxenprogrammer <emmy1000okello@gmail.com>
2021-08-01 04:09:53 +0300

    Feat(wear): add react-routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,77 @@
-function App() {
+/* eslint-disable no-unused-vars */
+import { Link, Route, Router, Switch } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import Login from './containers/Login';
+import Profile from './components/Profile';
+import Register from './containers/Register';
+import { clearMessage } from "./redux/actions/mesage";
+import { history } from "./helpers/history";
+import { logout } from "./redux/actions/auth";
+
+const App = () => {
+  const { user: currentUser } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    history.listen((location) => {
+      dispatch(clearMessage()); // clear message when changing location
+    });
+  }, [dispatch]);
+  
+  const logOut = () => {
+    dispatch(logout());
+  };
+  
+  
   return (
-    <div className="App">
-      Developer Wear
-    </div>
+    <Router history={history}>
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to={"/"} className="navbar-brand">
+            Sudo Wear
+          </Link>
+
+          {currentUser ? (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/profile"} className="nav-link">
+                  {currentUser.username}
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a href="/login" className="nav-link" onClick={logOut}>
+                  LogOut
+                </a>
+              </li>
+            </div>
+          ) : (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/login"} className="nav-link">
+                  Login
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to={"/register"} className="nav-link">
+                  Sign Up
+                </Link>
+              </li>
+            </div>
+          )}
+        </nav>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/login"]} component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/profile" component={Profile} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
