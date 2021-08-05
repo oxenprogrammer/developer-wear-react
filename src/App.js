92e501237e8d2e79f1f74469d470ffdf51ea62aa
oxenprogrammer <emmy1000okello@gmail.com>
2021-08-05 10:38:08 +0300

    Feat(home): add three columns

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Link, Route, Router, Switch } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import { Home } from "./components/Home";
 import Login from "./containers/Login";
 import Register from "./containers/Register";
 import Shirt from "./containers/Shirt";
@@ -10,8 +11,58 @@ import Shirts from "./containers/Shirts";
 import { clearMessage } from "./redux/actions/mesage";
 import { history } from "./helpers/history";
 import { logout } from "./redux/actions/auth";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    position: "absolute",
+    width: "100%",
+    display: "flex",
+    flexGrow: 1,
+    flexDirection: "column",
+  },
+  navbar: {
+    position: "relative",
+    display: "flex",
+    justifyContent: "space-between",
+    zIndex: "2",
+    padding: "2rem 1rem",
+    backgroundColor: "#212121",
+  },
+  navbarBrand: {
+    display: "inherit",
+    alignItems: "center",
+    textDecoration: "none",
+    fontWeight: "bold",
+    color: "#ED3170",
+    textTransform: "capitalize",
+    fontSize: "200%",
+    paddingLeft: "2.4rem",
+    "@media(max-width: 480px)": {
+      width: "120%",
+    },
+  },
+  navLink: {
+    display: "inherit",
+    alignItems: "center",
+    paddingRight: "2.4rem",
+  },
+  navItem: {
+    textDecoration: "none",
+    padding: "0.4rem 0.6rem",
+    margin: "0 0.6rem",
+    borderRadius: "1.2rem",
+    color: "#ffffff",
+    textTransform: "uppercase",
+    fontWeight: "bold",
+    "&:hover": {
+      backgroundColor: "#7B7A76",
+    },
+  },
+}));
 
 const App = () => {
+  const classes = useStyles();
   const { user: currentUser } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
@@ -27,43 +78,32 @@ const App = () => {
 
   return (
     <Router history={history}>
-      <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <Link to={"/"} className="navbar-brand">
+      <div className={classes.root}>
+        <nav className={classes.navbar}>
+          <Link to={"/"} className={classes.navbarBrand}>
             Sudo Wear
           </Link>
 
           {currentUser ? (
-            <div className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link to={"/shirts"} className="nav-link">
-                  Shirt Detail
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to={"#"} className="nav-link">
-                  {currentUser.username}
-                </Link>
-              </li>
-              <li className="nav-item">
-                <a href="/login" className="nav-link" onClick={logOut}>
-                  LogOut
-                </a>
-              </li>
+            <div className={classes.navLink}>
+              <Link to={"/shirts"} className={classes.navItem}>
+                Shirt Detail
+              </Link>
+              <Link to={"#"} className={classes.navItem}>
+                {currentUser.username}
+              </Link>
+              <Link to={"/login"} className={classes.navItem} onClick={logOut}>
+                LogOut
+              </Link>
             </div>
           ) : (
-            <div className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link to={"/login"} className="nav-link">
-                  Login
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link to={"/register"} className="nav-link">
-                  Sign Up
-                </Link>
-              </li>
+            <div className={classes.navLink}>
+              <Link to={"/login"} className={classes.navItem}>
+                Login
+              </Link>
+              <Link to={"/register"} className={classes.navItem}>
+                Sign Up
+              </Link>
             </div>
           )}
         </nav>
@@ -71,7 +111,8 @@ const App = () => {
         <div className="container mt-3">
           <Switch>
             <Route exact path="/shirts/:id" component={Shirt} />
-            <Route exact path={["/", "/login"]} component={Login} />
+            <Route exact path={["/", "/home"]} component={Home} />
+            <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/shirts" component={Shirts} />
           </Switch>
---
+++ src/components/Home.js
@@ -0,0 +1,186 @@
+import { Button, Grid, Typography } from "@material-ui/core";
+
+import backgroundImage from "../assets/img/swag.png";
+import girl from "../assets/img/naava.png";
+import { makeStyles } from "@material-ui/core";
+import tee from '../assets/img/tee.png';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    position: "absolute",
+    width: "100%",
+    display: "flex",
+    flexGrow: 1,
+    flexDirection: "column",
+    alignContent: "center",
+    alignItems: "center",
+  },
+  background: {
+    width: "100%",
+    backgroundColor: "#212121",
+  },
+  columnOne: {
+    display: "flex",
+    justifyContent: "flex-end",
+    marginRight: "4rem",
+  },
+  topImage: {
+    width: "30%",
+    "@media(max-width: 600px)": {
+      width: "60%",
+    },
+  },
+  topTag: {
+    color: "#ffffff",
+    paddingLeft: "3.6rem",
+    position: "absolute",
+    top: "3.6rem",
+    fontWeight: "bolder",
+    fontSize: "2rem",
+  },
+  pTag: {
+    color: "#ffffff",
+    paddingLeft: "3.6rem",
+    position: "absolute",
+    top: "8.6rem",
+  },
+  columnTwo: {
+    paddingTop: "2rem",
+    textAlign: "center",
+  },
+  nerds: {
+    textTransform: "uppercase",
+    fontWeight: "bold",
+    color: "#7A7B77",
+  },
+  smart: {
+    fontSize: "2.4rem",
+    fontWeight: "bold",
+  },
+  backgroundTwo: {
+    display: "grid",
+    gridTemplateColumns: "50% 50%",
+    justifyContent: "space-around",
+    padding: "2rem 3.6rem",
+    "@media(max-width: 719px)": {
+      gridTemplateColumns: "auto",
+    },
+  },
+  columnTwoimage: {
+    display: "flex",
+    justifyContent: "flex-end",
+    "@media(max-width: 719px)": {
+      justifyContent: "center",
+    },
+  },
+  secondImage: {
+    width: "60%",
+  },
+  secondTag: {
+    fontWeight: "bolder",
+    fontSize: "2rem",
+    textAlign: "left",
+    paddingTop: "2.8rem",
+    lineHeight: "-0.2px",
+    "@media(max-width: 768px)": {
+      textAlign: "center",
+    },
+  },
+  secondTagSpan: {
+    fontSize: "1rem",
+    lineHeight: "-0.2px",
+    textAlign: "left",
+    "@media(max-width: 768px)": {
+      textAlign: "center",
+    },
+  },
+  dumbButton: {
+    backgroundColor: "#ED5201",
+    color: "#ffffff",
+    borderRadius: "1.2rem",
+    padding: "0.6rem 1.2rem",
+    fontWeight: "bold",
+    margin: "1.6rem 0",
+    display: 'flex',
+    "@media(max-width: 768px)": {
+      margin: '1.6rem auto',
+    },
+  },
+  columnThree: {
+    backgroundColor: '#E7F1FE',
+  }
+}));
+
+export const Home = () => {
+  const classes = useStyles();
+
+  return (
+    <Grid container className={classes.root}>
+      <Grid item className={classes.background}>
+        <div className={classes.columnOne}>
+          <img
+            className={classes.topImage}
+            src={backgroundImage}
+            alt={"man on a developer t-shirt"}
+          />
+        </div>
+        <Typography className={classes.topTag}>Developers T-Shirts</Typography>
+        <Typography className={classes.pTag}>
+          If you recognise the words on the tees then you’re root.
+          <br /> $ sudo --call 0755850709 <br />$ sudo wear Based in Kampala
+        </Typography>
+      </Grid>
+      <Grid item className={classes.columnTwo}>
+        <article>
+          <Typography className={classes.nerds}>
+            Printed by Nerds for Nerds
+          </Typography>
+          <Typography className={classes.smart}>
+            Tees for World Best Developers
+          </Typography>
+        </article>
+        <article className={classes.backgroundTwo}>
+          <div>
+            <Typography className={classes.secondTag}>
+              Change the World.
+            </Typography>
+            <Typography className={classes.secondTagSpan}>
+              You will look really smart in these Tees, Code will just flow and
+              you will not event realize the day coming to an end.
+            </Typography>
+            <Button className={classes.dumbButton}>LEARN MORE</Button>
+          </div>
+          <div>
+            <img
+              className={classes.secondImage}
+              src={tee}
+              alt={"t-shirt with change the world code tag"}
+            />
+          </div>
+        </article>
+      </Grid>
+      <Grid item className={[classes.columnTwo, classes.columnThree]}>
+        <article className={classes.backgroundTwo}>
+          <div>
+            <img
+              className={classes.secondImage}
+              src={girl}
+              alt={"girl wearing a stackoverflow developer t-shirt"}
+            />
+          </div>
+          <div>
+            <Typography className={classes.secondTag}>
+              Best in Sale Programmers Tees.
+            </Typography>
+            <Typography className={classes.secondTagSpan}>
+              You will look really smart in these Tees, Code will just flow and
+              you will not event realize the day coming to an end. It is the
+              look you have been waiting, worth a lifetime.
+            </Typography>
+            <Button className={classes.dumbButton}>LEARN MORE</Button>
+          </div>
+        </article>
+      </Grid>
+    </Grid>
+  );
+};
