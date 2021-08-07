c7f14e5901f877f71211a6ef57b46f0a940890e8
oxenprogrammer <emmy1000okello@gmail.com>
2021-08-07 23:36:26 +0300

    Perf(config): update base url

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 require('dotenv').config()
 
-const API_URL=BASE_URL;
+const API_URL='https://gentle-thicket-58693.herokuapp.com/api/v1/';
 
 const register = (username, email, password, password_confirmation) => {
   return axios.post(API_URL + "users", {
